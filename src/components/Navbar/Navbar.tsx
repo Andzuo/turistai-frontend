06030ec42a92eb259d33200c99fa6a4b29789fc1
Andzuo <alucasm02@gmail.com>
2024-08-25 15:00:09 -0300

    feat: add logic a Login bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,37 @@
+import { useState, useEffect } from "react";
 import { Logo } from "../assets/Logo";
 import s from "./Navbar.module.css";
+import { useNavigate } from "react-router-dom";
+
+const UserIcon = () => <img src="" alt="User Icon" className={s.userIcon} />;
 
 export const Navbar = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const token = localStorage.getItem("acessToken");
+		setIsLoggedIn(!!token);
+	}, []);
+
+	const loginnav = () => {
+		navigate("/login");
+	};
 	return (
 		<header className={s.navbarRoot}>
 			<nav className={s.navbar_container}>
 				<div className={s.navbar_logo}>
 					<Logo />
 				</div>
-				{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-				<button className={s.navbar_login}>Login</button>
+				<div className={s.navbar_actions}>
+					{isLoggedIn ? (
+						<UserIcon /> // Mostra o ícone do usuário quando logado
+					) : (
+						<button onClick={loginnav} type="button" className={s.navbar_login}>
+							Login
+						</button>
+					)}
+				</div>
 			</nav>
 		</header>
 	);
