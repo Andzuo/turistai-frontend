9af982c74716af2be6619a17f2014cc7dbff311d
Andzuo <alucasm02@gmail.com>
2024-08-25 10:18:28 -0300

    feat: add style login page

--- src/pages/sign-in/Login.tsx
+++ src/pages/sign-in/Login.tsx
@@ -1,35 +1,35 @@
-import s from "./Cadastro.module.css";
+import s from "./Login.module.css";
 
-export const Cadastro = () => {
+export const Login = () => {
 	return (
-		<div className={s.cadastro}>
-			<div className={s.cadastro__container}>
-				<h1 className={s.cadastro__title}>Cadastro</h1>
-				<form className={s.cadastro__form}>
-					<div className={s.cadastro__form__group}>
-						<label htmlFor="name" className={s.cadastro__form__label}>
+		<div className={s.login}>
+			<div className={s.login__container}>
+				<h1 className={s.login__title}>Login</h1>
+				<form className={s.login__form}>
+					<div className={s.login__form__group}>
+						<label htmlFor="name" className={s.login__form__label}>
 							Nome
 						</label>
 						<input
 							type="text"
 							id="name"
-							className={s.cadastro__form__input}
+							className={s.login__form__input}
 							placeholder="Digite seu nome"
 						/>
 					</div>
-					<div className={s.cadastro__form__group}>
-						<label htmlFor="password" className={s.cadastro__form__label}>
+					<div className={s.login__form__group}>
+						<label htmlFor="password" className={s.login__form__label}>
 							Senha
 						</label>
 						<input
 							type="password"
 							id="password"
-							className={s.cadastro__form__input}
+							className={s.login__form__input}
 							placeholder="Digite sua senha"
 						/>
 					</div>
-					<button type="submit" className={s.cadastro__form__button}>
-						Cadastrar
+					<button type="submit" className={s.login__form__button}>
+						Logar
 					</button>
 				</form>
 			</div>
