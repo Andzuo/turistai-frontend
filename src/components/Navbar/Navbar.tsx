0771ec87324e2975c203040d4a6b1135f53ef982
Andzuo <alucasm02@gmail.com>
2024-08-28 19:17:50 -0300

    fix: resolve

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 import Profile from "../Profile/Profile";
 
 export const Navbar = () => {
-	const [open, setOpen] = useState(false);
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const navigate = useNavigate();
 
