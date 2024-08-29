242c5f969a755e0f793008304a7faaeead9ef63c
Andzuo <alucasm02@gmail.com>
2024-08-28 21:32:44 -0300

    cleaning Project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/sign-in/Login";
 import { Cadastro } from "./pages/sign-up/Cadastro";
 import { Home } from "./pages/home/Home";
-import { Perfil } from "./pages/Perfil/Perfil";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -16,7 +15,6 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/cadastro" element={<Cadastro />} />
-					<Route path="/profile" element={<Perfil />} />
 				</Routes>
 			</div>
 		</>
