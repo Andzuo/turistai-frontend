7c6ad84159e0c3880cc9c5c166e711dad29737af
Andzuo <alucasm02@gmail.com>
2024-08-30 23:25:15 -0300

    feat: add roadMapComponent logic and style

--- src/components/RoadMapComponent/RoadMapComponent.tsx
+++ src/components/RoadMapComponent/RoadMapComponent.tsx
@@ -1,37 +1,51 @@
 import type React from "react";
-import s from "./RoadMapComponent.module.css";
 import type { RoadMapData } from "../../services/RoadMapService";
+
+import s from "./RoadMapComponent.module.css";
 
 interface RoadMapComponentProps {
 	roadMap: RoadMapData;
 }
 
-const RoadMapComponent: React.FC<RoadMapComponentProps> = ({ roadMap }) => {
+export const RoadMapComponent: React.FC<RoadMapComponentProps> = ({
+	roadMap,
+}) => {
 	return (
 		<div className={s.roadMap}>
-			<h4>{roadMap.title}</h4>
-			<p>{roadMap.addres}</p>
-			{roadMap.image && (
-				<img
-					src={`http://localhost:8080/api/roadmap/image/${roadMap.image}`}
-					alt={roadMap.title}
-					className={s.roadMapImage}
-				/>
-			)}
-			<p>Visitado: {roadMap.visited ? "Sim" : "Não"}</p>
-			{roadMap.comments && roadMap.comments.length > 0 && (
-				<div>
-					<h5>Comentários:</h5>
-					<ul>
-						{roadMap.comments.map((comment, index) => (
-							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-							<li key={index}>{comment}</li>
-						))}
-					</ul>
+			<div className={s.roadMap__container}>
+				<div className={s.roadMap__content__image}>
+					{roadMap.image && (
+						<img
+							src={roadMap.image}
+							alt={roadMap.title}
+							className={s.roadMap__content__image__img}
+						/>
+					)}
+					<h3 className={s.roadMap__container__title}>{roadMap.title}</h3>
 				</div>
-			)}
+				<p className={s.roadMap_container__addres}>
+					{`Endereço: ${roadMap.addres}`}
+				</p>
+				<div className={s.roadMap__container__checkbox}>
+					<label
+						className={s.roadMap__container__checkbox__visited}
+						htmlFor="visited"
+					>
+						Visitou?
+					</label>
+					<input
+						className={s.roadMap__container__checkbox__visited__input}
+						id="visited"
+						type="checkbox"
+					/>
+				</div>
+				<input
+					id="comment"
+					type="text"
+					placeholder="adicione um comentário ao local"
+				/>
+			</div>
+			<hr style={{ margin: "10px 0", width: "100%" }} />
 		</div>
 	);
 };
-
-export default RoadMapComponent;
