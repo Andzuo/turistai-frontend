0a94033701be63d6e71cdaf8515579e0e15005b6
Andzuo <alucasm02@gmail.com>
2024-09-01 12:04:53 -0300

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 
+import s from "./Footer.module.css";
+
 export const Footer = () => {
 	return (
-		<footer>
+		<footer className={s.footer}>
 			<p>
 				Desnvolvido por{" "}
-				<Link to={"https://www.linkedin.com/in/anderson-medeiros-dev/"}>
+				<Link
+					about="_blank"
+					to={"https://www.linkedin.com/in/anderson-medeiros-dev/"}
+				>
 					Anderson Medeiros
 				</Link>
 			</p>
