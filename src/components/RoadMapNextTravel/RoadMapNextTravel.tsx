6afbf653a142c3f435289bf70acb41de5feb7926
Andzuo <alucasm02@gmail.com>
2024-09-01 09:40:43 -0300

    feat: add RoadMapNextTravel and Styles

--- src/components/RoadMapNextTravel/RoadMapNextTravel.tsx
+++ src/components/RoadMapNextTravel/RoadMapNextTravel.tsx
@@ -1,3 +1,17 @@
-export const RoadMapNextTravel = () => {
-	return <div>RoadMapNextTravel</div>;
+import { RoadMapList } from "../RoadMapList/RoadMapList";
+
+import s from "./RoadMapNextTravel.module.css";
+
+interface RoadMapNextTravelProps {
+	travelId: number;
+}
+
+export const RoadMapNextTravel: React.FC<RoadMapNextTravelProps> = ({
+	travelId,
+}) => {
+	return (
+		<div className={s.roadMapNextTravel}>
+			<RoadMapList travelId={travelId} />
+		</div>
+	);
 };
